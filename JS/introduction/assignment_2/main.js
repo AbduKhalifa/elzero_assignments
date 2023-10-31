792c74c18f4f65a152e83a197b1f574863552167
AbduKhalifa <mezoozem2002@gmail.com>
2023-10-31 22:10:20 +0200

    add some assignments

---
+++ JS/introduction/assignment_2/main.js
@@ -0,0 +1,8 @@
+
+document.querySelector("h1").style = `
+color: blue;
+font-size: 80px;
+font-weight: bold;
+text-align: center;
+font-family: Arial
+`
---
+++ JS/introduction/assignment_3/main.js
@@ -0,0 +1,8 @@
+
+
+console.log(
+    "%c Hello %cWorld! %cNehahaha",
+    "color:#f00;font-size:40px",
+    "font-weight:bold;font-size:40px",
+    "background-color:#00f;color:#fff;font-size:40px"
+);
